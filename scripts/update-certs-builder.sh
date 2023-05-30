277ad0fb247f4578dd9c812709af62a46b147f22
Adam Carpenter <adamc@jlab.org>
2023-05-30 11:55:13 -0400

    Update for minor shell optimizations.  * Add quotes to prevent unnecessary internal operations  * $@ -> $*

--- scripts/update-certs-builder.sh
+++ scripts/update-certs-builder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # All arguments are treated as certificates
-CUSTOM_CRT_URL="$@"
+CUSTOM_CRT_URL="$*"
 
 if [ -z "${CUSTOM_CRT_URL}" ] ; then
   echo "No custom certs passed.  Nothing to update"
@@ -10,16 +10,16 @@ fi
 
 for cert in $CUSTOM_CRT_URL
 do
-  echo Downloading $cert
-  name=$(echo $cert | rev | cut -f1 -d"/" | rev | cut -f1 -d'.') || exit 1
-  wget -O /usr/local/share/ca-certificates/custom-${name}.crt $cert || exit 1
+  echo Downloading "$cert"
+  name=$(echo "$cert" | rev | cut -f1 -d"/" | rev | cut -f1 -d'.') || exit 1
+  wget -O "/usr/local/share/ca-certificates/custom-${name}.crt" "$cert" || exit 1
 done
 
 update-ca-certificates || exit 1
 
 for cert in $CUSTOM_CRT_URL
 do
-  echo Importing $cert
-  name=$(echo $cert | rev | cut -f1 -d"/" | rev | cut -f1 -d'.') || exit 1
-  keytool -import -alias custom_${name} -file /usr/local/share/ca-certificates/custom-${name}.crt -cacerts -storepass changeit -noprompt || exit 1
+  echo Importing "$cert"
+  name=$(echo "$cert" | rev | cut -f1 -d"/" | rev | cut -f1 -d'.') || exit 1
+  keytool -import -alias "custom_${name}" -file "/usr/local/share/ca-certificates/custom-${name}.crt" -cacerts -storepass changeit -noprompt || exit 1
 done
