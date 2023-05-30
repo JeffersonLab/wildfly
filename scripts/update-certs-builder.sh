f96127e0114fefe87252f13353f96079fc57490f
Adam Carpenter <adamc@jlab.org>
2023-05-30 11:52:24 -0400

    OPTIONAL_CERT_ARG unneeded in this image

--- scripts/update-certs-builder.sh
+++ scripts/update-certs-builder.sh
@@ -23,7 +23,3 @@ do
   name=$(echo $cert | rev | cut -f1 -d"/" | rev | cut -f1 -d'.') || exit 1
   keytool -import -alias custom_${name} -file /usr/local/share/ca-certificates/custom-${name}.crt -cacerts -storepass changeit -noprompt || exit 1
 done
-
-# This line was in the Dockerfile, but I can't tell that it did anything.  Leaving as a reminder.
-# export OPTIONAL_CERT_ARG=--cert=/etc/ssl/certs/ca-certificates.crt || exit 1
-
