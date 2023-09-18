6d1938f4ad823a756dc530006a21bc24a1aaafc1
ryans <ryans@jlab.org>
2023-09-18 10:01:24 -0400

    Pass TAG/VERSION in as argument

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-FUNCTIONS=(deploy)
-
 VARIABLES=(DOWNLOAD_URL
            WAR_FILE
            WILDFLY_APP_HOME)
 
 if [[ $# -eq 0 ]] ; then
-    echo "Usage: $0 [var file] <optional function>"
+    echo "Usage: $0 [var file] <GitHub tag/version>"
     echo "The var file arg should be the path to a file with bash variables that will be sourced."
-    echo "The optional function name arg if provided is the sole function to call, else all functions are invoked sequentially."
-    printf 'Variables: '
-    printf '%s ' "${VARIABLES[@]}"
-    printf '\n'
-    printf 'Functions: '
-    printf '%s ' "${FUNCTIONS[@]}"
     printf '\n'
     exit 0
 fi
@@ -22,8 +14,18 @@ fi
 if [ ! -z "$1" ] && [ -f "$1" ]
 then
 echo "Loading environment $1"
+
+if [ -z "$2" ]
+then
+echo "Version/Tag required"
+exit 0
+fi
+
+TAG=$2
+
 . $1
 fi
+
 
 # Verify expected env set:
 for i in "${!VARIABLES[@]}"; do
@@ -40,17 +42,4 @@ wget ${DOWNLOAD_URL}
 ${WILDFLY_CLI_PATH} -c "deploy --force /tmp/${WAR_FILE}"
 }
 
-if [ ! -z "$2" ]
-then
-  echo "------------------------"
-  echo "$2"
-  echo "------------------------"
-  $2
-else
-for i in "${!FUNCTIONS[@]}"; do
-  echo "------------------------"
-  echo "${FUNCTIONS[$i]}"
-  echo "------------------------"
-  ${FUNCTIONS[$i]};
-done
-fi
+deploy
